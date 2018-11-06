8ead27af022e6ff2c46be93984043f5c0610387b
Vijay <vijayganganapalli@gmail.com>
2018-11-06 07:43:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_widgets
+
+A flutter widgets training app.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DogApp());
+}
+
+class DogApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Dog App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Yellow Lab"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              DogName("Rocky"),
+              SizedBox(height: 8.0),
+              DogName("Lyla"),
+              SizedBox(height: 8.0),
+              DogName("Nico"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DogName extends StatelessWidget {
+  final String name;
+
+  const DogName(this.name);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.lightBlueAccent),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(name),
+      ),
+    );
+  }
+}
