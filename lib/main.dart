4187740f734350846108b3468a08deebdc8805b0
Vijay <vijayganganapalli@gmail.com>
2018-11-06 08:30:35 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,42 +8,12 @@ class DogApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My Dog App",
+      title: "Hello World!",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Yellow Lab"),
-        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              DogName("Rocky"),
-              SizedBox(height: 8.0),
-              DogName("Lyla"),
-              SizedBox(height: 8.0),
-              DogName("Nico"),
-            ],
-          ),
+          child: Text("Hello World!"),
         ),
       ),
     );
   }
 }
-
-class DogName extends StatelessWidget {
-  final String name;
-
-  const DogName(this.name);
-  
-  @override
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.lightBlueAccent),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text("Rocky"),
-      ),
-    );
-  }
-}
-
